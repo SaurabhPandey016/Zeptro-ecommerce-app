ab517462aa50b0d212ffb644108ef04358e1cee6
saurabh pandey <wesaysaurabh@gmail.com>
2026-03-07 11:46:28 +0530

    added social media links

--- zeptro-app-frontend/src/components/Footer.jsx
+++ zeptro-app-frontend/src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 // import Logo from '../assets/Logo.png'
-import { FaFacebook, FaInstagram, FaPinterest, FaTwitterSquare } from 'react-icons/fa'
+import { FaGithub, FaLinkedin, FaInstagram, FaTwitterSquare } from 'react-icons/fa'
 
 const Footer = () => {
+  const navigate = useNavigate();
   return (
     <footer className='bg-gray-900 text-gray-200 py-10'>
       <div className='max-w-7xl mx-auto px-4 md:flex md:justify-between'>
@@ -22,21 +23,52 @@ const Footer = () => {
         <div className='mb-6 md:mb-0'>
             <h3 className='text-xl font-semibold'>Customer Service</h3>
             <ul className='mt-2 text-sm space-y-2'>
-                <li>Contact Us</li>
-                <li>Shipping & Returns</li>
-                <li>FAQs</li>
-                <li>Order Tracking</li>
-                <li>Size Guide</li>
+                <li onClick={()=>navigate('/contact')}  className='cursor-pointer'>Contact Us</li>
+                <li onClick={()=>navigate('/')}  className='cursor-pointer'>Shipping & Returns</li>
+                <li onClick={()=>navigate('/')}  className='cursor-pointer'>FAQs</li>
+                <li onClick={()=>navigate('/')}  className='cursor-pointer'>Order Tracking</li>
+                <li onClick={()=>navigate('/')}  className='cursor-pointer'>Size Guide</li>
             </ul>
         </div>
         {/* social media links */}
         <div className='mb-6 md:mb-0'>
             <h3 className='text-xl font-semibold'>Follow Us</h3>
             <div className='flex space-x-4 mt-2'>
-                <FaFacebook/>
-                <FaInstagram/>
-                <FaTwitterSquare/>
-                <FaPinterest/>
+              <a
+                href="https://www.linkedin.com/in/saurabhpandey-/"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ textDecoration: "none" }}
+              >
+                <FaLinkedin className='cursor-pointer'/> {/* Clicking this will open the link */}
+              </a>
+
+              <a
+                href="https://github.com/SaurabhPandey016"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ textDecoration: "none" }}
+              >
+                <FaGithub className='cursor-pointer'/> {/* Clicking this will open the link */}
+              </a>
+                
+              <a
+                href="https://www.instagram.com/mr._bunny_/"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ textDecoration: "none" }}
+              >
+                <FaInstagram className='cursor-pointer'/> {/* Clicking this will open the link */}
+              </a>
+              
+              <a
+                href="https://x.com/Saurabh_Pandey_/"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ textDecoration: "none" }}
+              >
+                <FaTwitterSquare className='cursor-pointer'/> {/* Clicking this will open the link */}
+              </a>
             </div>
         </div>
         {/* newsletter subscription */}
