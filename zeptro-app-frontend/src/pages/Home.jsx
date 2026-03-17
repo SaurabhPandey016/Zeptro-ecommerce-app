90ed809abc62036195d12e072cdc0e078d062865
saurabh pandey <wesaysaurabh@gmail.com>
2026-03-17 17:34:18 +0530

    added few improvements-1

--- zeptro-app-frontend/src/pages/Home.jsx
+++ zeptro-app-frontend/src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
       try {
         const res = await axios.get("https://zeptro-ecommerce-app.onrender.com/products");
         if(res.status === 200) {
-          // setIsDataLoaded(true);
+          setIsDataLoaded(true);
         }
       } catch (error) {
         console.error("Error fetching data:", error);
